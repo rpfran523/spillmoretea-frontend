4ff89d90b3f391852fbf7b7c0eaae2ef3bc942fd
Shawn F <shawnf@Shawns-Air.lan>
2025-07-27 19:50:03 -0400

    Fix Next.js build configuration

---
+++ out/_next/static/chunks/pages/index-694bbdfad41d715d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[332],{2936:(_,e,n)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return n(9287)}])},9287:(_,e,n)=>{"use strict";n.r(e),n.d(e,{default:()=>r});var u=n(7876);function r(){return(0,u.jsx)("h1",{children:"Spill More Tea"})}}},_=>{_.O(0,[636,593,792],()=>_(_.s=2936)),_N_E=_.O()}]);
